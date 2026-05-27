8b7777bb0be634b4d88d7a9c9a78c2e7902e6b76
SoucupB <bogdan.soucup@gmail.com>
2026-05-27 22:48:23 +0300

    WIP

--- Kite/Sources/main.cpp
+++ Kite/Sources/main.cpp
@@ -2,12 +2,43 @@
 #include "Unit.h"
 #include "ActionManager.h"
 #include "EETwa.h"
+#include "Player.h"
+
+void processEnemyAction(Unit unit, Action action);
 
 void onChangeAction(Unit unit, Action newAction, Action oldAction) {
-  if(!unit_IsSelf(unit)) {
+  processEnemyAction(unit, newAction);
+}
+
+Point getOpositePosition(Point center, Point rotator) {
+  return (Point) {
+    .x = 2 * center.x - rotator.x,
+    .y = 2 * center.y - rotator.y,
+  };
+}
+
+void processSelfUnitAndTarget(Unit selfUnit, Unit enemyUnit) {
+  const float currentEnemyRange = unit_Range(enemyUnit);
+  const float currentSelfRange = unit_Range(selfUnit);
+  if(currentEnemyRange > currentSelfRange && unit_Distance(selfUnit, enemyUnit) < currentSelfRange * 0.85f) {
     return ;
   }
-  eeTa_FilePrintf("Unit changed action from %p to %p\n", oldAction.type, newAction.type);
+  Point movingPoint = getOpositePosition(unit_Point_Position(selfUnit), unit_Point_Position(enemyUnit));
+  unit_Action(selfUnit, movingPoint, UNIT_MOVE);
+}
+
+void processEnemyAction(Unit unit, Action action) {
+  if(unit_IsSelf(unit) || ply_AreAllies(ply_GetPlayer(unit), ply_Self())) {
+    return ;
+  }
+  if(action.type != ACTION_ATTACK_TARGET) {
+    return ;
+  }
+  Unit target = action.target;
+  if(!unit_IsSelf(target)) {
+    return ;
+  }
+  processSelfUnitAndTarget(target, unit);
 }
 
 void bt_OnFrame() {
